cfaadbfb80dad839829bd3fe660f3cc5818a1741
dineshgpillai <dinesh.g.pillai@gmail.com>
2017-01-30 20:22:45 +0000

    Adding docker files

--- spring-boot-docker-example/src/main/java/com/innovation/main/Application.java
+++ spring-boot-docker-example/src/main/java/com/innovation/main/Application.java
@@ -1,12 +1,21 @@
 package com.innovation.main;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Application {
-	
-	public static void main(String[] args){
-		
-	}
+	@RequestMapping("/")
+    public String home() {
+        return "Hello Dinesh You are in Docker World";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
 
 }
