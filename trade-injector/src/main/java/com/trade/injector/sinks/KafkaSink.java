c17d88076ee05cc77d2e040cdf60728951214951
dineshgpillai <dinesh.g.pillai@gmail.com>
2017-05-22 07:51:17 +0100

    Setting up Kafka layer

---
+++ trade-injector/src/main/java/com/trade/injector/sinks/KafkaSink.java
@@ -0,0 +1,14 @@
+package com.trade.injector.sinks;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaSink implements ISink {
+
+	@Override
+	public void writeTo(String url, String message) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
